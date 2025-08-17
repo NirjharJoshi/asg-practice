2a449b36ae98c078b9d7a01b0933080bccc797b7
Nirjhar Joshi <njupkiet@gmail.com>
2025-08-17 16:09:29 +0530

    remove cluster logic

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,20 @@
-import cluster from "node:cluster";
-import os from "node:os";
 import express from "express";
 
-const numOfCpus = os.cpus().length;
+const PORT = 8000;
+const app = express();
 
-if (cluster.isPrimary) {
-  console.log(`machine has total number of cpus: ${numOfCpus}`);
-  console.log(`Primary ${process.pid} is running`);
-  // numOfCpus - 2
-  for (let i = 1; i <= numOfCpus; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} is died`);
-    console.log("starting new worker");
-    cluster.fork();
+app.get("/first", async (req, res, next) => {
+  return res.status(200).json({
+    message: `response from first route handled by worker ${process.pid}`,
   });
-} else {
-  const PORT = 8000;
-  const app = express();
+});
 
-  app.get("/first", async (req, res, next) => {
-    return res.status(200).json({
-      message: `response from first route handled by worker ${process.pid}`,
-    });
+app.get("/second", async (req, res, next) => {
+  return res.status(200).json({
+    message: `response from second route handled by worker ${process.pid}`,
   });
+});
 
-  app.get("/second", async (req, res, next) => {
-    return res.status(200).json({
-      message: `response from second route handled by worker ${process.pid}`,
-    });
-  });
-
-  app.listen(PORT, () => {
-    console.log(`app listening on port: ${PORT}`);
-  });
-
-  console.log(`worker ${process.pid} started`);
-}
+app.listen(PORT, () => {
+  console.log(`app listening on port: ${PORT}`);
+});
